fb9102c9c35d955fdc6dabcc8904fd430ef36978
prodelasieste <167758397+prodelasieste@users.noreply.github.com>
2026-02-14 18:40:12 +0100

    Add files via upload

--- script/label.gd
+++ script/label.gd
@@ -1,26 +1,22 @@
 extends Label
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 
 
 
 func _on_texture_button_pressed() -> void:
-	#jordan pour conectée le bouton avec le label g utilisée les signaux a droite c la fleche vert a cotée dla fonction
-	#evite de lenlevée stp cvar jlavait enlevée par accident c chiant a voirs
-	# Replace with function body.
+	var buttontexture = get_parent().get_node("TextureButton")
 	var Melenchonadd= int(text)+1
 	text = str(Melenchonadd)
-	#la variable text c juste le texte du label enfte
-	#TextureButton.Texture_normal = load("res://Melenchon2.png")
+	print("Melenchon cliquée", Melenchonadd)
 	
-	print("Melenchon cliquée", Melenchonadd) # Replace with function body.
+	var chooseMelenchon = ceil(randf_range(0, 2))
+	print (chooseMelenchon)
+	if chooseMelenchon == 1: 
+		buttontexture.texture_normal = load("res://texture/melenchon/Melenchonid1.png")
+	elif chooseMelenchon == 2:
+		buttontexture.texture_normal = load("res://texture/melenchon/Melenchonid2.png")
+	else:
+		print("tes con !? ho lis le code idiot")
+	
+	
